3b368663401a901cb97c4c7ed268955edcc33fd2
Tim Camper <twcamper@thoughtworks.com>
2014-02-17 05:21:20 -0600

    possessive pronoun phrase

--- lib/grammar_cards/syntax.rb
+++ lib/grammar_cards/syntax.rb
@@ -3,7 +3,15 @@
 module GrammarCards
   module Syntax
     module PossessiveNounPhrase
-
+      def spanish(noun, owner)
+        phrase = GrammarCards::Words::DefiniteArticle.new(noun.gender, noun.number).spanish
+        phrase += " "
+        phrase += noun.spanish
+        phrase += " de"
+        phrase += " "
+        phrase += GrammarCards::Words::PossessivePronoun.spanish(owner[:person], owner[:number], owner[:gender])
+      end
+      extend PossessiveNounPhrase
     end
 
     class PossessiveAdjectivalNounPhrase
--- spec/grammar_cards/syntax_spec.rb
+++ spec/grammar_cards/syntax_spec.rb
@@ -5,7 +5,17 @@ module GrammarCards
   module Syntax
 
     describe PossessiveNounPhrase do
+      it "should return Spanish for feminine plural noun, 3rd masculine plural owner" do
+        noun = GrammarCards::Words::Noun.new("pluma f pen", :p)
+        phrase = PossessiveNounPhrase.spanish(noun, :person => 3, :gender => :m, :number => :p)
+        expect(phrase).to eq 'las plumas de ellos'
+      end
 
+      it "should return Spanish for  masculine singular noun, 2nd singular owner" do
+        noun = GrammarCards::Words::Noun.new("libro m book", :s)
+        phrase = PossessiveNounPhrase.spanish(noun, :person => 2, :number => :s)
+        expect(phrase).to eq 'el libro de Ud.'
+      end
     end
 
     describe PossessiveAdjectivalNounPhrase do
