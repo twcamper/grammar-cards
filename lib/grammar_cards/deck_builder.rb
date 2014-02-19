ebcb17c1d26563c7e88bcaa300a8e15de069443c
Tim Camper <twcamper@thoughtworks.com>
2014-02-18 18:41:07 -0600

    shuffle structures after read from disk

--- lib/grammar_cards/deck_builder.rb
+++ lib/grammar_cards/deck_builder.rb
@@ -8,9 +8,20 @@ module GrammarCards
     DATA_PATH = File.expand_path('../data', File.dirname($0))
     NOUN_FILE = File.join(DATA_PATH, "nombres-sostantivos.yml")
     POSSESSIVE_ADJECTIVE_STRUCTURE_FILE = File.join(DATA_PATH, "adjetivos-posesivos.yml")
+
+    def shuffle_structures
+      ordered_structures = Psych.load_file(POSSESSIVE_ADJECTIVE_STRUCTURE_FILE)
+
+      shuffled = []
+      until ordered_structures.empty?
+        shuffled << ordered_structures.delete_at(rand(ordered_structures.size))
+      end
+      shuffled
+    end
+
     def build
       noun_data = Psych.load_file(NOUN_FILE)
-      structure_data = Psych.load_file(POSSESSIVE_ADJECTIVE_STRUCTURE_FILE)
+      structure_data = shuffle_structures
       deck = []
       structure_data.each_with_index do |record, i|
         noun_data[i][:num] = record[0][:num]
