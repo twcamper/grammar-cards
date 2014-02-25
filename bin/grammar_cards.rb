38635e39215359509a359f4d0cd32f8f6ecef5ed
Tim Camper <twcamper@thoughtworks.com>
2014-02-25 11:07:07 -0600

    take command line args to specify decks

--- bin/grammar_cards.rb
+++ bin/grammar_cards.rb
@@ -1,9 +1,13 @@
 #! /usr/bin/env ruby
 require_relative '../lib/grammar_cards'
 
-GrammarCards::Deck.run(
-  GrammarCards::Deck.build(
-    :PossessiveAdjectives,
-    :Numbers
-)
-)
+module GrammarCards
+  module CLI
+p   DECKS = if ARGV.empty?
+              GrammarCards::Deck::Builders.constants
+            else
+              ARGV.map {|arg| arg.to_sym}
+            end
+  end
+end
+GrammarCards::Deck.run(GrammarCards::Deck.build( *GrammarCards::CLI::DECKS ))
